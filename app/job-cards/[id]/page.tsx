6d93f8366a038995698947d7e2364cc12d7f1e1c
krys2fa <christopher.k.amanor@gmail.com>
2025-09-20 16:07:24 +0000

    fix: resolve issues on details page for small scale job card

--- app/job-cards/[id]/page.tsx
+++ app/job-cards/[id]/page.tsx
@@ -313,7 +313,7 @@ function JobCardDetailPage() {
               Edit Job Card
             </Link>
           )}
-          {/* {canDelete && (
+          {canDelete && (
             <button
               onClick={handleDelete}
               className="inline-flex items-center px-4 py-2 border border-red-300 shadow-sm text-sm font-medium rounded-md text-red-700 bg-white hover:bg-red-50"
@@ -679,7 +679,7 @@ function JobCardDetailPage() {
                 {/* View Valuation */}
                 Assay Report Analysis
               </Link>
-              <Link
+               <Link
                 href={`/job-cards/${id}/assays/certificate`}
                 className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
               >
